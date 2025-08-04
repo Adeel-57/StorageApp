cee2208f6af60eb9a1a3cac67e10b7acc69b05b2
Adeel-57 <adeelcreationx557@gmail.com>
2025-08-04 17:53:38 +0500

    db updated

--- dataBase/db.js
+++ dataBase/db.js
@@ -4,7 +4,7 @@ const dbPath = process.env.DB_PATH;
 async function conectToDb(cb) {
   try {
     await mongoose.connect(dbPath);
-
+    console.log("db");
     // #*Connect to mongodb*#
     // client = await MongoClient.connect(
     //   "mongodb://admin:admin123@localhost:27017/admin?replicaSet=localReplica"
