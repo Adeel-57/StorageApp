8719bafb60870fd704311f84bc0757502d8bc74a
Muhammad Adeel <168734847+Adeel-57@users.noreply.github.com>
2025-09-22 14:42:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ app.use((req, res, next) => {
   conectToDb(async (err) => {
     if (!err) {
       connection = true;
-      app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-      });
+      // app.listen(PORT, () => {
+      //   console.log(`Server running on port ${PORT}`);
+      // });
     } else {
       console.log("Connection to db failed");
       process.exit(0);
